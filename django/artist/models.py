c6836eaaab09ad3872a64ebcb76c1cc6395cab27
YunHo <shdbsghxg@gmail.com>
2018-02-06 17:10:12 +0900

    [add]:shell-django_extensions, [update]:Artist.models.py to work properly in shell_plus

--- django/artist/models.py
+++ django/artist/models.py
@@ -16,11 +16,11 @@ class Artist(models.Model):
         (BLOOD_TYPE_O, 'O형'),
         (BLOOD_TYPE_OTHER, '기타'),
     )
-    img_profile = models.ImageField('프로필 이미지', upload_to='artist')
+    img_profile = models.ImageField('프로필 이미지', upload_to='artist', blank=True)
     name = models.CharField('이름', max_length=50)
     real_name = models.CharField('본명', max_length=30, blank=True)
     nationality = models.CharField('국적', max_length=50)
-    birth_date = models.DateTimeField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
     constellation = models.CharField('별자리', max_length=30)
     blood_type = models.CharField('혈액형', max_length=1, choices=CHOICES_BLOOD_TYPE, blank=True)
     intro = models.TextField('소개', blank=True)
