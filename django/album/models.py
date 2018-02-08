89727f83025a3c1dfae67079e5bb6770560d243b
YunHo <shdbsghxg@gmail.com>
2018-02-08 11:24:05 +0900

    [add]: album.models.py - Album clss

--- django/album/models.py
+++ django/album/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from artist.models import Artist
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField('앨범명', max_length=50)
+    img_cover = models.ImageField('커버 이미지', upload_to='album', blank=True)
+    artists = models.ManyToManyField(Artist, verbose_name='artist list')
+    release_date = models.DateField()
+
+    @property
+    def genre(self):
+        return ''
+
+    def __str__(self):
+        return f'{title} [{artists}]'.format(
+            title=self.title,
+            artists=', '.join(self.artists.values_list('name', flat=True))
+        )
+
